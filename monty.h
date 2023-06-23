81ccbd9eed393fbb3e7b1d99ee7a19907f912a80
jasmineH-D <mellyasminita2000@gmail.com>
2023-06-22 19:52:51 -0700

    tasks

--- monty.h
+++ monty.h
@@ -36,3 +36,48 @@ typedef struct instruction_s
 	char *opcode;
 	void (*func)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+/* op functions handler */
+void (*op_functions(char *str))(stack_t **stack, unsigned int numL);
+
+/* free stack */
+void freesStack(stack_t *stack);
+
+/* op functions*/
+void pushOP(stack_t **stack, unsigned int line_number);
+void pallOP(stack_t **stack, unsigned int line_number);
+void pintOP(stack_t **stack, unsigned int line_number);
+void popOP(stack_t **stack, unsigned int line_number);
+void swapOP(stack_t **stack, unsigned int line_number);
+void addOP(stack_t **stack, unsigned int line_number);
+void nopOP(stack_t **stack, unsigned int line_number);
+
+/* error handlers */
+void fileErr(void);
+void useErr(void);
+void mallocErr(stack_t **stack);
+void opcodeErr(stack_t **stack, unsigned int numL);
+void addErr(stack_t **stack, unsigned int numL);
+void pushErr(stack_t **stack, unsigned int numL);
+void pintErr(stack_t **stack, unsigned int numL);
+void swapErr(stack_t **stack, unsigned int numL);
+void popErr(stack_t **stack, unsigned int numL);
+
+
+/* shared vars */
+/**
+ * struct valStack - shared a variables in the program
+ * @n: value
+ * @file: file to read.
+ * @code: tokenized a command
+ * @mode: 0 for stack, 1 for queue
+ * Description: contains info about everything in the program
+ */
+struct valStack
+{
+	char *n;
+	char *code;
+	int mode;
+	char *file;
+} valStack;
+
+#endif
